68ce0cec188a4f53f6e91f935ca3fe4e8ca1fac2
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-25 14:08:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+attr_accessor :total
 
+def initialize(discount)
+  @total = 0
+  @discount = discount
+end
+
+
+def total
+end
+
+def add_item
+end
+
+def apply_discount
+end
+
+def items
+end
+
+def void_last_transaction
+end
+
+
+end
